8f83a06ac2cf853565fd43740685553c42d6bec3
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2020-12-23 15:23:46 -0500

    added Vercel app as client origin in config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000, 
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DB_URL || 'postgresql://user_name@localhost/database_name', // DB_URL fallback goes here
-  CLIENT_ORIGIN: '',
+  CLIENT_ORIGIN: 'https://rei-search-v2-client.vercel.app/',
   STREETVIEW_API_KEY: process.env.STREETVIEW_API_KEY,
   STREETVIEW_SIGNATURE: process.env.STREETVIEW_SIGNATURE,
 }
