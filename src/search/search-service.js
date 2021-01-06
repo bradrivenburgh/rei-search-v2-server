8413acb2808fc0eac06f46f195f86b72c098be93
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-01-06 14:49:16 -0500

    streetview images now load correctly

--- src/search/search-service.js
+++ src/search/search-service.js
@@ -5,10 +5,9 @@ const SearchService = {
       .from("properties")
       .whereRaw(
         `ST_DistanceSphere(geometry(properties.location),
-        ST_MakePoint(${searchLocation})) <= ?`, [10000.0]
+        ST_MakePoint(${searchLocation})) <= ?`, [10000.00]
       )
       .limit(50)
-      .offset(50)
       .then((properties) => {
         return properties;
       });
