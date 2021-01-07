3c56f267710ac36cf3e934d5e1d920492b9facdb
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-01-07 17:31:08 -0500

    fixing ssl issues

--- src/server.js
+++ src/server.js
@@ -5,9 +5,7 @@ const { PORT, DATABASE_URL } = require('./config');
 const db = knex({
   client: 'pg',
   connection: DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: false 
 });
 
 app.set('db', db);
