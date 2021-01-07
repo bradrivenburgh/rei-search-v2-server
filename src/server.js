b025f65186d726697208f341732831d0f19b5feb
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-01-07 12:14:42 -0500

    made db connection use ssl

--- src/server.js
+++ src/server.js
@@ -4,7 +4,8 @@ const { PORT, DATABASE_URL } = require('./config');
 
 const db = knex({
   client: 'pg',
-  connection: DATABASE_URL
+  connection: DATABASE_URL,
+  ssl: true
 });
 
 app.set('db', db);
