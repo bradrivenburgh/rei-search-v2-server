549dd4c7341ee358259e63ad3acc8fc5d6da7d76
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-01-11 17:05:50 -0500

    cleanded up search-endpoint.test.js

--- test/search-endpoint.test.js
+++ test/search-endpoint.test.js
@@ -2,8 +2,8 @@ const { expect } = require("chai");
 const knex = require("knex");
 const supertest = require("supertest");
 const app = require("../src/app");
-const { transformStats } = require('../src/search/transformStats');
-const { cannedResponse, makeMaliciousSearch, countyPepValues } = require("./search.fixtures");
+const { transformStats } = require("../src/search/transformStats");
+const { countyPepValues } = require("./search.fixtures");
 
 describe("Search Endpoint", function () {
   let db;
@@ -21,71 +21,53 @@ describe("Search Endpoint", function () {
   after("disconnect from db", () => db.destroy());
 
   describe(`GET /api/search`, () => {
-    // beforeEach(() => {
-    //   this.get = sinon.stub(supertest(), 'get');
-    // });
+    let api_response;
+    console.log = () => {};
 
-    // afterEach(() => {
-    //   supertest().get.restore();
-    // });
+    // context(`Given an empty query string`, () => {
+    it("responds with 200 and defaults to statistics and properties in Philadelphia", () => {
+      return supertest(app)
+        .get("/api/search")
+        .query('address=""')
+        .expect(200)
+        .then((res) => {
+          expect(
+            res.body.apiStatistics.economic[1]["CT"]
+          ).to.have.lengthOf.at.least(1);
+          expect(
+            res.body.apiStatistics.demographic[1]["CT"]
+          ).to.have.lengthOf.at.least(1);
+          expect(res.body.properties).to.have.lengthOf.at.least(1);
+          expect(res.body.properties[1].address.city).to.equal("Philadelphia");
+          expect(res.body.properties[1].address.state).to.equal("PA");
 
-    context(`Given an empty query string`, () => {
-      it("responds with 200 and defaults to statistics and properties in Philadelphia", () => {
-        return supertest(app)
-          .get("/api/search")
-          .query('address=""')
-          .expect(200)
-          .then((res) => {
-            expect(res.body.apiStatistics.economic[1]["CT"]).to.have.lengthOf.at.least(1);
-            expect(res.body.apiStatistics.demographic[1]["CT"]).to.have.lengthOf.at.least(1);
-            expect(res.body.properties).to.have.lengthOf.at.least(1);
-            expect(res.body.properties[1].address.city).to.equal("Philadelphia");
-            expect(res.body.properties[1].address.state).to.equal("PA");
+          api_response = res;
+        });
+    }).timeout(15000);
 
-            context('Given a valid response', () => {        
-              const statistics = {
-                msa: res.body.msa.features[0].properties,
-                county: res.body.county.features[0].properties,
-                countyPep: countyPepValues,
-                tract: res.body.tract.features[0].properties,
-              };
-        
-              it('transformStats transforms the Census data correctly', () => {
-                expect(res.body.apiStatistics).to.eql(transformStats(statistics))
-              })
-            });
-      
-          });
-      }).timeout(15000);
-
-      // it.only('responds with 200 using stub', (done) => {
-      //   this.get = sinon.stub(supertest(app), 'get');
-      //   this.get.yields(null, cannedResponse.all.success.res, JSON.stringify(cannedResponse.all.success.body));
-
-      //   supertest(app)
-      //     .get('/api/search')
-      //     .query('address=""')
-      //     .expect(200)
-      //     .end(done)
-
-      // }).timeout(15000)
+    it("transformStats transforms the Census data correctly", () => {
+      const data = api_response.body;
+      const statistics = {
+        msa: data.msa.features[0].properties,
+        county: data.county.features[0].properties,
+        countyPep: countyPepValues,
+        tract: data.tract.features[0].properties,
+      };
+      expect(data.apiStatistics).to.eql(transformStats(statistics));
     });
-
-    context('Given the query "Medford, NJ"', () => {
-      it('responds with properties within 10k of the given coordinates in NJ', () => {
-        return supertest(app)
-          .get('/api/search')
-          .query('address=Medford, NJ')
-          .expect(200)
-          .then((res) => {
-            const properties = res.body.properties;
-            const state = properties.some(property => 
-              property.address.state === "NJ"
-            );
-            expect(state).to.equal(true);
-          })
-      }).timeout(15000)
-    })
-
   });
+
+  it("responds with properties within 10k of the given coordinates in NJ", () => {
+    return supertest(app)
+      .get("/api/search")
+      .query("address=Medford, NJ")
+      .expect(200)
+      .then((res) => {
+        const properties = res.body.properties;
+        const state = properties.some(
+          (property) => property.address.state === "NJ"
+        );
+        expect(state).to.equal(true);
+      });
+  }).timeout(15000);
 });
